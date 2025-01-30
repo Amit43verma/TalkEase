b3c9fe2c189862d251456a705400103b82fc341b
Amit Verma <144679426+Amit43verma@users.noreply.github.com>
2025-01-30 12:28:14 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,7 +25,13 @@ const __dirname = path.resolve();
 // 	}
 // ));
 
-app.use(express.json());
+// app.use(express.json());
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+
+
 app.use(cookieParser());
 
 // to allow cross origin requests
